1c90cfb4e5d0cbbd959925939f906c27d242a20b
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-07-02 14:10:32 -0400

    Create FormatTestNGXML.js

---
+++ parsers/FormatTestNGXML.js
@@ -0,0 +1,89 @@
+const PulseSdk = require('@qasymphony/pulse-sdk');
+const request = require('request');
+const xml2js = require('xml2js');
+const { Webhooks } = require('@qasymphony/pulse-sdk');
+
+exports.handler = function ({ event: body, constants, triggers }, context, callback) {
+    function emitEvent(name, payload) {
+        let t = triggers.find(t => t.name === name);
+        return t && new Webhooks().invoke(t, payload);
+    }
+
+        const xml2js = require("xml2js");
+
+        var payload = body;
+        var testResults = payload.result;
+        var projectId = payload.projectId;
+        var cycleId = payload.testcycle;
+        var testLogs = [];
+        var timestamp = new Date();
+        var requiresDecode = payload.requiresDecode;
+
+        if(requiresDecode == 'true') {
+            var xmlString = decodeURI(testResults);
+            xmlString = xmlString.replace(/`/g, '&');
+        }
+
+        xml2js.parseString(xmlString, {
+            preserveChildrenOrder: true,
+            explicitArray: false,
+            explicitChildren: false
+        }, function (err, result) {
+            if (err) {
+                emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { Error: "Unexpected Error Parsing XML Document: " + err }); 
+            } else {
+                var testsuites = Array.isArray(result['testng-results'].suite.test) ? result['testng-results'].suite.test : [result['testng-results'].suite.test];
+                testsuites.forEach(function(ts) {
+                    suiteName = ts.$.name;
+                    var testcases = Array.isArray(ts.class) ? ts.class : [ts.class];
+                    testcases.forEach(function(obj) {
+                        var className = obj.$.name;
+                        var testMethods = Array.isArray(obj['test-method']) ? obj['test-method'] : [obj['test-method']];
+                        testMethods.forEach(function(tm) {
+                            var methodName = tm.$.name;
+                            var methodStatus = tm.$.status;
+                            var startTime = tm.$['started-at'];
+                            var endTime = tm.$['finished-at'];
+                            var note = '';
+                            var stack = '';
+                            if (methodStatus == 'FAIL') {
+                            note = tm.exception.message;
+                            stack = tm.exception['full-stacktrace'];
+                            }
+                            var exe_start_date = timestamp;
+                            var exe_end_date = timestamp;
+                            exe_end_date.setSeconds(exe_start_date.getSeconds() + (Math.floor(obj.$.time || 0)));
+                            var testLog = {
+                            status: methodStatus,
+                            name: methodName,
+                            attachments: [],
+                            note: note,
+                            exe_start_date: exe_start_date.toISOString(),
+                            exe_end_date: exe_start_date.toISOString(),
+                            automation_content: className + "#" + methodName,
+                            module_names: [suiteName, className, methodName]
+                            };
+                            if (stack !== '') {
+                            testLog.attachments.push({
+                                name: `${methodName}.txt`,
+                                data: Buffer.from(stack).toString("base64"),
+                                content_type: "text/plain"
+                            });
+                            }
+                            //testLog.attachments.push(payload.consoleOutput[0]);
+                            testLogs.push(testLog);
+                        });
+                    });
+                });
+            }
+        });
+
+        var formattedResults = {
+            "projectId" : projectId,
+            "test-cycle" : cycleId,
+            "logs" : testLogs
+        };
+
+        emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { ResultsFormatSuccess: "Results formatted successfully for project"}); 
+        emitEvent('<INSERT NAME OF UPDATE QTEST RULE HERE>', formattedResults );
+    }
