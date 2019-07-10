b43e5d968a6270f92187bef6783b3d318d64f0c7
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-07-10 11:35:10 -0400

    Create deliver.ps1

---
+++ delivery/windows-powershell/deliver.ps1
@@ -0,0 +1,15 @@
+$url = '<ENTER YOUR PARSER PULSE WEBHOOK HERE>'
+
+Set-Location -Path "<ENTER YOUR RESULTS DIRECTORY HERE>"
+
+$payload = (Get-Content "<ENTER YOUR RESULTS FILE NAME HERE>" -Raw)
+
+$body = @{
+ 'projectId' = '<ENTER YOUR QTEST PROJECT ID HERE>'
+ 'test-cycle' = '<ENTER YOUR QTEST TEST CYCLE ID HERE>'
+ 'requiresDecode' = 'true'
+ 'result' = $payload
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Body $body -Method 'Post' -Uri $url
