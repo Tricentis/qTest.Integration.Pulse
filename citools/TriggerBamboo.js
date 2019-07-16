af593acab80d0b47c86a2a62a21435497b324044
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-07-16 08:54:01 -0400

    Update TriggerBamboo.js

--- citools/TriggerBamboo.js
+++ citools/TriggerBamboo.js
@@ -3,13 +3,8 @@ const request = require('request');
 const xml2js = require('xml2js');
 
 exports.handler = async function({ event, constants, triggers }, context, callback) {
-  var bambooProjectCode = body.commits[0].message;
 
-  //Either place project code in constants or pass in an argument from the GitHub payload
-  // var url = "http://" + constants.BambooURL + '/rest/api/latest/queue/' + constants.BambooProjectandPlan + '?stage&executeAllStages&customRevision&os_authType=basic';
-
-  var url = "http://" + constants.BambooURL + '/rest/api/latest/queue/' + bambooProjectCode + '?stage&executeAllStages&customRevision&os_authType=basic';
-
+  var url = "http://" + constants.BambooURL + '/rest/api/latest/queue/' + constants.bambooProjectCode + '?stage&executeAllStages&customRevision&os_authType=basic';
 
   var opts = {
       url: url,
@@ -24,10 +19,10 @@ exports.handler = async function({ event, constants, triggers }, context, callba
   request.post(opts, function(err, res, bd) {
           if(err) {
               Promise.reject(err);
-              emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { GitPushBambooProject: "Git Push but commit message is not project name code" }); 
+              emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { GitPushBambooProject: "Git Push but commit message is not project name code: " + constants.bambooProjectCode}); 
           }
           else {
-              emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { BambooCallSuccess: "Bamboo Build just kicked off for this plan " + bambooProjectCode }); 
+              emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { BambooCallSuccess: "Bamboo Build just kicked off for this plan: " + constants.bambooProjectCode }); 
 
           }
   })
