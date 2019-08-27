e64ed67193bb8bbd78c36e9c8a50e3a2710d0375
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-08-27 10:49:51 -0400

    Updated code with modern, functional rule

--- parsers/FormatJUnitXML.js
+++ parsers/FormatJUnitXML.js
@@ -8,8 +8,6 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
         let t = triggers.find(t => t.name === name);
         return t && new Webhooks().invoke(t, payload);
     }
-
-        const xml2js = require("xml2js");
                 
         var payload = body;
         var testResults = payload.result; 
@@ -23,59 +21,89 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
             xmlString = xmlString.replace(/`/g, '&');
         }
 
-        xml2js.parseString(xmlString, {
+        console.log(xmlString);
+
+        var parseString = require('xml2js').parseString;
+        var startTime = '';
+        var endTime = '';
+        var lastEndTime = 0;
+
+        parseString(testResults, {
             preserveChildrenOrder: true,
             explicitArray: false,
             explicitChildren: false
         }, function (err, result) {
             if (err) {
-                emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { Error: "Unexpected Error Parsing XML Document: " + err }); 
+                emitEvent('SlackEvent', { Error: "Unexpected Error Parsing XML Document: " + err }); 
+                console.log(err);
             } else {
-                var testsuites = Array.isArray(result['testsuites']) ? result['testsuites'] : [result['testsuites']];
+                var testsuites = Array.isArray(result.testsuites['testsuite']) ? result.testsuites['testsuite'] : [result.testsuites['testsuite']];
                 testsuites.forEach(function(testsuite) {
+                    lastEndTime = 0;
                     suiteName = testsuite.$.name;
-                    console.log(suiteName)
+                    console.log('Suite Name: ' + suiteName)
                     var testcases = Array.isArray(testsuite.testcase) ? testsuite.testcase : [testsuite.testcase];
                     testcases.forEach(function(testcase) {
-                        var className = testcase.$.name;
-                        console.log(className)
-                        /*
-                        var testFailure = Array.isArray(obj['failure']) ? obj['failure'] : [obj['failure']];
-                        testMethods.forEach(function(tm) {
-                            var methodName = tm.$.name;
-                            var methodStatus = tm.$.status;
-                            var startTime = tm.$['started-at'];
-                            var endTime = tm.$['finished-at'];
-                            var note = '';
-                            var stack = '';
-                            if (methodStatus == 'FAIL') {
-                            note = tm.exception.message;
-                            stack = tm.exception['full-stacktrace'];
+                        var classArray = [];
+                        classArray = testcase.$.name.replace('=>', ':').split(':');
+                        var depth = classArray.length;
+                        var className = classArray[(depth - 1)];
+                        var moduleNames = [];
+                        var moduleCount = 0;
+                        classArray.forEach(function(folder) {
+                            if(moduleCount < (depth - 1)) {
+                                moduleNames.push(folder.trim());
+                                moduleCount++;
                             }
-                            var exe_start_date = timestamp;
-                            var exe_end_date = timestamp;
-                            exe_end_date.setSeconds(exe_start_date.getSeconds() + (Math.floor(obj.$.time || 0)));
-                            var testLog = {
-                            status: methodStatus,
-                            name: methodName,
+                        })
+                        if(moduleNames.length == 0) {
+                            moduleNames.push(suiteName);
+                        }
+                        console.log('Class Name: ' + className)
+                        var classStatus = 'passed';
+                        if(lastEndTime == 0) {
+                            startTime = new Date(Date.parse(testsuite.$.timestamp)).toISOString();
+                        } else {
+                            startTime = lastEndTime;
+                        }
+                        interim = new Date(Date.parse(startTime)).getSeconds() + parseFloat(testcase.$.time);
+                        endTime = new Date(Date.parse(startTime)).setSeconds(interim);
+                        endTime = new Date(endTime).toISOString();
+
+                        var note = '';
+                        var stack = '';
+                        var testFailure = Array.isArray(testcase.failure) ? testcase.failure : [testcase.failure];
+                        testFailure.forEach(function(failure) {
+                            if(failure !== undefined) {
+                                console.log(failure.$.type)
+                                note = failure.$.type + ': ' + failure.$.message;
+                                console.log(failure._)
+                                stack = failure._;
+                                classStatus = 'failed';
+                            }
+                        });
+                        console.log(classStatus);
+
+                        var testLog = {
+                            status: classStatus,
+                            name: className,
                             attachments: [],
                             note: note,
-                            exe_start_date: exe_start_date.toISOString(),
-                            exe_end_date: exe_start_date.toISOString(),
-                            automation_content: className + "#" + methodName,
-                            module_names: [suiteName, className, methodName]
-                            };
-                            if (stack !== '') {
-                            testLog.attachments.push({
-                                name: `${methodName}.txt`,
-                                data: Buffer.from(stack).toString("base64"),
-                                content_type: "text/plain"
-                            });
-                            }
-                            //testLog.attachments.push(payload.consoleOutput[0]);
-                            testLogs.push(testLog);
+                            exe_start_date: startTime,
+                            exe_end_date: endTime,
+                            automation_content: htmlEntities(className),
+                            module_names: moduleNames
+                        };
+                        if (stack !== '') {
+                        testLog.attachments.push({
+                            name: `${className}.txt`,
+                            data: Buffer.from(stack).toString("base64"),
+                            content_type: "text/plain"
                         });
-                        */
+                        }
+                        //testLog.attachments.push(payload.consoleOutput[0]);
+                        testLogs.push(testLog);
+                        lastEndTime = endTime;
                     });
                 });
             }
@@ -87,7 +115,11 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
             "logs" : testLogs
         };
 
-        emitEvent('<INSERT NAME OF CHATOPS INTEGRATION RULE HERE>', { ResultsFormatSuccess: "Results formatted successfully for project"}); 
-        emitEvent('<INSERT NAME OF UPDATE QTEST RULE HERE>', formattedResults );
+        emitEvent('SlackEvent', { ResultsFormatSuccess: "Results formatted successfully for project"}); 
+        emitEvent('UpdateQTestWithFormattedResults', formattedResults );
 
 };
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
