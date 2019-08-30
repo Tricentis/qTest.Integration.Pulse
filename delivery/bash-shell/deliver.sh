071aa9a33b3161ab88b775fad89c431c31436302
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-08-30 10:04:30 -0400

    fix for curl character limit
    Curl couldn't handle cucumber results files with embedded images very well, so this is the fix to accommodate for that.

--- delivery/bash-shell/deliver.sh
+++ delivery/bash-shell/deliver.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-cd "<ENTER YOUR RESULTS DIRECTORY HERE>"
+cd "<ENTER YOUR RESULTS DIRECTORY HERE>" # CHANGE THIS TO POINT TO YOUR CUCUMBER RESULTS FILE
 
-#Read in file
-logs=$(<<ENTER YOUR RESULTS FILENAME HERE>)
+echo '{ "test-cycle" : <ENTER YOUR QTEST TEST CYCLE ID HERE>, "result" : ' > payload.json
+cat <ENTER YOUR RESULTS FILENAME HERE> >> payload.json
+echo ', "projectId" : <ENTER YOUR QTEST PROJECT ID HERE> }' >> payload.json
+
 
 curl -X POST \
-  <ENTER YOUR PULSE PARSER WEBHOOK URL HERE> \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -d '{
-    "projectId" : <ENTER YOUR QTEST PROJECT ID HERE>,
-    "test-cycle" : <ENTER YOUR QTEST TEST CYCLE ID HERE>,
-    "result" : '"$logs"'
-}'
+ <ENTER YOUR PULSE PARSER WEBHOOK URL HERE> \
+ -H 'cache-control: no-cache' \
+ -H 'content-type: application/json' \
+ -d @payload.json
