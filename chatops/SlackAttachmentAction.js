6d9af6c32eeaa407e93a34b7b760efd031f46828
Chris Peters <40035198+qaschris@users.noreply.github.com>
2019-09-11 12:35:03 -0400

    Create SlackAttachmentAction.js

---
+++ chatops/SlackAttachmentAction.js
@@ -0,0 +1,40 @@
+const PulseSdk = require('@qasymphony/pulse-sdk');
+const request = require('request');
+const xml2js = require('xml2js');
+
+// This is to attach large payloads to a Slack file attachment to bypass 
+// the Slack message character limitation as well as the Pulse console.log
+// length limitation.  Requires constant "SlackToken" to be created.  See
+// https://api.slack.com/custom-integrations/legacy-tokens for details.
+
+exports.handler = function({ event: body, constants, triggers }, context, callback) {
+    function emitEvent(name, payload) {
+      let t = triggers.find((t) => t.name === name);
+      return t && new Webhooks().invoke(t, payload);
+    };
+
+    
+    var payload = body;
+
+    //console.log(payload);
+
+    var options = {
+        method: 'POST',
+        url: 'https://slack.com/api/files.upload',
+        formData: {
+            enctype: "application/x-www-form-urlencoded",
+            token: constants.SlackToken,
+            title: 'payload.json',
+            filename: 'payload.json',
+            filetype: 'javascript',
+            channels: 'GFT63D059',
+            content: JSON.stringify(payload)
+        },
+    }
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(`statusCode: ${response.statusCode}`);
+        console.log(JSON.parse(response.body));
+    });
+}
