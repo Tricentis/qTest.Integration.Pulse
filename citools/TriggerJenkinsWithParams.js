4b76bf49e5bfb85d4ae19209134af589a57b05ad
Chris Peters <chrispeters@qasymphony.net>
2020-01-06 17:31:16 -0500

    Adding node.js script for parser payload delivery

---
+++ citools/TriggerJenkinsWithParams.js
@@ -0,0 +1,57 @@
+/**
+ * call source: Repository (Bitbucket, Github, Gitlab, etc), Scenario Action
+ * payload example: { tag: "tag value" }
+ * constants:
+ *  JenkinsUserName: admin
+ *  JenkinsAPIToken: fa96ad2f-5e1c-4562-a14d-98a94ba9bab1
+ *  JenkinsURL: jenkins.yourdomain.com:8080
+ *  JenkinsParamJob: CucumberBuildJob
+ *  JenkinsJobToken: fa96ad2f-5e1c-4562-a14d-98a94ba9bab1
+ * outputs:
+ *  The specified build job with parameters will be triggered in Jenkins
+ */
+
+const request = require('request');
+const { Webhooks } = require('@qasymphony/pulse-sdk');
+
+exports.handler = function ({ event: body, constants, triggers }, context, callback) {
+    function emitEvent(name, payload) {
+        let t = triggers.find(t => t.name === name);
+        return t && new Webhooks().invoke(t, payload);
+    }
+
+        var payload = body;
+        var parameter = payload.tag;
+
+        var url = "http://" + constants.JenkinsUserName + ":" + constants.JenkinsAPIToken + "@" + 
+        constants.JenkinsURL + '/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)';
+
+        request.get({url:url, insecure: true}, function(err, response, body) {
+            if(!err) {
+                var crumb = body.split(":")[1];
+
+                var joburl = "http://" + constants.JenkinsUserName + ":" + constants.JenkinsAPIToken + "@" + 
+                        constants.JenkinsURL + "/job/" + constants.JenkinsParamJob + "/buildWithParameters?token=" + constants.JenkinsJobToken + "&Tag=" + parameter
+                var opts = {
+                    url: joburl,
+                    insecure: true,
+                    contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+                    headers: {
+                        "Jenkins-Crumb": crumb
+                    }
+                }
+
+                request.post(opts, function(err, res, bd) {
+                    if(!err) {
+                        emitEvent('ChatOpsEvent', { message: "Jenkins Build just kicked off for project: " + constants.JenkinsParamJob });
+                    }
+                    else {
+                        emitEvent('ChatOpsEvent', { message: "Jenkins Build kickoff failed for project: " + constants.JenkinsParamJob + " & Error: " + err });
+                    }
+                });
+            }
+            else {
+                emitEvent('ChatOpsEvent', { message: "Jenkins Build kickoff failed for project: " + constants.JenkinsParamJob + " & Error: " + err  }); 
+            }
+        });
+}
---
+++ delivery/node.js/delivery.js
@@ -0,0 +1,49 @@
+/**
+ * Notes for Automation Host 2.3.2 and earlier:
+ * This script currently requires the 'request' and 'uuid' node.js modules
+ * to be manually copied to the host installation directory:
+ * Ex: C:\[host directory]\build\qautomation\runner\node_modules
+ * Uncommenting the next line determines where the host is looking for node modules:
+ * console.log(module.paths);
+ */
+
+const fs = require('fs');
+const path = require('path');
+const request = require('request'); // dependent upon 'uuid'
+
+const pulseUri = '';                // Pulse parser webhook endpoint
+const projectId = '';               // target qTest Project ID
+const cycleId = '';                 // target qTest Test Cycle ID
+
+var result = '';
+
+let resultsPath = 'C:\\path\\to\\results\\filename.ext';
+
+try {
+	result = fs.readFileSync(resultsPath, 'utf8');
+    console.log('Read file successfully.');    
+} catch(e) {
+    console.log('Error: ', e.stack);
+}
+
+var opts = {
+    url: pulseUri,
+    json: true,
+    body: {
+    	projectId: projectId,
+        'test-cycle': cycleId,
+        requiresDecode: 'true',
+        result: result
+    }
+};
+
+return request.post(opts, function (err, response, resbody) {
+    if (err) {
+        Promise.reject(err);
+    }
+    else {
+        //console.log(response);
+        //console.log(resbody);
+        Promise.resolve("Uploaded results successfully.");
+    }
+});
