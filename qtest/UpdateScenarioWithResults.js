2f3a71e385c8fa9102c5d836a25f6ce9bd25640a
Chris Peters <chrispeters@qasymphony.net>
2020-03-30 12:32:21 -0400

    String matching fix for scenarioColors rule

--- qtest/UpdateScenarioWithResults.js
+++ qtest/UpdateScenarioWithResults.js
@@ -7,8 +7,12 @@ const StepSdk = {
 }
 
 const Steps = {
-    updateStepResults(stepSdk, name, status) {
-        return stepSdk.getSteps(`"${name}"`).then(steps => Promise.all(steps.map(step => stepSdk.updateStep(step.id, Object.assign(step, { status })))));
+    updateStepResults(stepSdk, name, status, issueId = null, keyword = null) {
+        return stepSdk.getSteps(name, keyword, issueId). // issueId only available from Pulse v9.2
+            then(steps => Promise.all(steps.map(step => stepSdk.updateStep(step.id, Object.assign(step, { status })))))
+            .catch(function (err) {
+                console.log('Error updating colors: ', err);
+            });
     }
 };
 
@@ -16,12 +20,15 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
     var payload = body;
     var testLogs = payload.logs;
 
-    stepSdk = StepSdk.getStepSdk(constants.qTestAPIToken, constants.SCENARIO_PROJECT_ID);
+    stepSdk = StepSdk.getStepSdk(constants.QTEST_TOKEN, constants.SCENARIO_PROJECT_ID);
     
     for (var res of testLogs) {
         for (var step of res["test_step_logs"]) {
-            var stepName = step.description;
+            // If for any reason you change the step description be certain to edit the following line to create a match for the Scenario SDK!!
+            var stepName = step.description.slice(step.description.indexOf(' ')).trim();
             var stepStatus = step.status;
+            var stepKeyword = step.keyword;
+            var issueId = step.issueId;
 
             // Undefined means no step definition existed and it should fail
             if (stepStatus == "undefined") {
@@ -32,7 +39,13 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
             stepStatus = stepStatus.toUpperCase();
 
             // Call the pulse API to update step results
-            Steps.updateStepResults(stepSdk, stepName, stepStatus);
+            try {
+                console.log("Calling updateStepResults with: " + JSON.stringify(stepSdk) + ", " + stepName + ", " + stepStatus + ", " + issueId + ", " + stepKeyword);
+                Steps.updateStepResults(stepSdk, stepName, stepStatus, issueId, stepKeyword);
+            }
+            catch {
+                console.log("Error calling updateStepResults");
+            }
         }
     }
 }
