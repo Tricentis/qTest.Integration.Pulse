271c8e5b7634fc9a324de1a65203535252f85749
Chris Peters <40035198+qaschris@users.noreply.github.com>
2024-09-13 10:51:40 -0400

    Update TestNGXML.js
    Updates for shipping large amounts of tests
    Expansion of housekeeping tasks to omit
    Attachment of failure stack traces
    Description property into note field (if exists)

--- parsers/TestNGXML.js
+++ parsers/TestNGXML.js
@@ -1,18 +1,23 @@
 const xml2js = require('xml2js');
 const { Webhooks } = require('@qasymphony/pulse-sdk');
 
-exports.handler = function ({ event: body, constants, triggers }, context, callback) {
+exports.handler = async function ({ event: body, constants, triggers }, context, callback) {
     function emitEvent(name, payload) {
         let t = triggers.find(t => t.name === name);
         return t ? new Webhooks().invoke(t, payload) : console.error(`[ERROR]: (emitEvent) Webhook named '${name}' not found.`);
     }
 
+    function convertUTCToISO(utcTimestamp) {
+        // Replace ' UTC' with 'Z' to convert to ISO 8601 format
+        const isoTimestamp = utcTimestamp.replace(' UTC', 'Z');
+        return new Date(isoTimestamp).toISOString();
+    }
+
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
     let payload = body;
     let projectId = payload.projectId;
     let cycleId = payload.testcycle;
-    let testLogs = [];
-    let testSteps = [];
-    let timestamp = new Date();
 
     let testResults = Buffer.from(payload.result, 'base64').toString('utf8');
 
@@ -20,7 +25,7 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
         preserveChildrenOrder: true,
         explicitArray: false,
         explicitChildren: false
-    }, function (err, result) {
+    }, async function (err, result) {
         if (err) {
             emitEvent('ChatOpsEvent', { message: "Error: Unexpected Error Parsing XML Document: " + err });
             console.log("Error: Unexpected Error Parsing XML Document: " + err );
@@ -40,94 +45,67 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
 
         console.log(`[INFO]: Processing ${testSuites.length} test suites.`);
 
-        testSuites.forEach(function(ts) {
+        for (const ts of testSuites) {
             let suiteName = ts.$.name;
             console.log('[INFO]: Suite Name: ', suiteName);
 
             // Check if 'class' array exists
             if (!ts.class) {
                 console.log('[INFO]: No test cases found in suite: ', suiteName);
-                return;
+                continue;
             }
 
-            let testCases = Array.isArray(ts.class) ? ts.class : [ts.class];
-            console.log(`[INFO]: Processing ${testCases.length} test cases.`);
+            let testClasses = Array.isArray(ts.class) ? ts.class : [ts.class];
+            console.log(`[INFO]: Processing ${testClasses.length} test cases.`);
+            let testLogs = [];
 
-            testCases.forEach(function(tc) {
+            for (const tc of testClasses) {
                 let className = tc.$.name;
                 console.log('[INFO]: Class Name: ', className);
-                let exe_start_date = timestamp;
-                let methodStatus = 'PASS';
-                testSteps = [];
+                let methodStatus;
+                testLogs = [];
+                let testSteps = [];
                 let order = 0;
 
                 // Check if 'test-method' array exists
                 if (!tc['test-method']) {
                     console.log('[INFO]: No test methods found in class: ', className);
-                    return;
+                    continue;
                 }
 
-                const invalidMethods = ['afterMethod', 'beforeMethod', 'beforeTest', 'beforeClass', 'afterClass', 'testBreakdown', 'Reset'];
+                // Below is an all-lowercase array of housekeeping methods that should not have results recorded in qTest.
+                const invalidMethods = ['endtest', 'setup', 'suitesetup', 'beforesuite', 'aftermethod', 'beforemethod', 'beforetest', 'beforeclass', 'afterclass', 'aftersuite', 'aftertest', 'testbreakdown', 'reset', 'teardown'];
                 let testMethods = Array.isArray(tc['test-method']) ? tc['test-method'] : [tc['test-method']];
                 console.log(`[INFO]: Processing ${testMethods.length} test methods.`);
 
-                testMethods.forEach(function(tm) {
+                for (const tm of testMethods) {
                     let methodName = tm.$.name;
-                    if(!invalidMethods.includes(methodName)) {
+                    if (!invalidMethods.includes(methodName.toLowerCase())) {
+                        testSteps = [];
                         console.log('[INFO]: Method Name: ', methodName);
                         methodStatus = tm.$.status;
+                        let automationContent = `${className}#${methodName}`; 
+                        let exe_start_date = convertUTCToISO(tm.$['started-at']);
+                        let exe_end_date = convertUTCToISO(tm.$['finished-at']);
+                        let methodDescription = tm.description || methodName;
                         let stackException;
-                        let reporterOutput;
                         let paramList = [];
-
-                        // Grab the stack trace if it is a failed test, since these only exist in failures
-                        if (methodStatus == 'FAIL') {
-                            stackException = tm.exception['full-stacktrace'];
-                        }
+                        let encodedMethodAttachment;
 
                         // Flatten the params structure into a plaintext list of params
                         if (tm.params && tm.params.param) {
                             let paramArray = Array.isArray(tm.params.param) ? tm.params.param : [tm.params.param];
                             console.log(`[INFO]: Processing ${paramArray.length} parameters.`);
-                            paramArray.forEach(function(param) {
-                                if (typeof param.value === 'object') {
-                                    console.log(`[INFO]: Processing parameter: ${JSON.stringify(param.value)}`);
-                                    paramList.push(JSON.stringify(param.value));
-                                } else if (typeof param.value === 'string') {
-                                    console.log(`[INFO]: Processing parameter: ${param.value.trim()}`);
-                                    paramList.push(param.value.trim());
-                                } else {
-                                    console.log(`[ERROR]: Unexpected param value type: ${typeof param.value}`);
-                                }
+                            paramArray.forEach(param => {
+                                paramList.push(
+                                    typeof param.value === 'object'
+                                        ? JSON.stringify(param.value)
+                                        : param.value.trim()
+                                );
                             });
-                            paramList = paramList.join('\n');
-                            console.log(paramList);
+                            paramList = '?' + paramList.join('&');
+                            automationContent += paramList; // Append the params to the automation content
                         }
-                        
-                        // Flatten the reporter-output structure into a plaintext list of reporter output
-                        console.log(`[INFO]: Processing reporter output entries.`);
-                        if (Array.isArray(tm['reporter-output'])) {
-                            let lines = tm['reporter-output'].reduce((acc, item) => {
-                                if (item.line) {
-                                    acc.push(...item.line.map(line => line.trim().replace('\n','')));
-                                }
-                                return acc;
-                            }, []);
-                        
-                            reporterOutput = lines.join('\n');
-                            //console.log(reporterOutput);
-                        } else if (tm['reporter-output'] && Array.isArray(tm['reporter-output'].line)) {
-                            reporterOutput = tm['reporter-output'].line.join('\n');
-                            //console.log(reporterOutput);
-                        } else if (tm['reporter-output'] && typeof tm['reporter-output'].line === 'string') {
-                            reporterOutput = tm['reporter-output'].line;
-                            //console.log(tm['reporter-output'].line);
-                        }
-
-                        // Mongle all these together into a single text file attachment, if they are populated
-                        let stepAttachment = `${paramList !== undefined ? 'Parameters:\n' + paramList + '\n\n' : ''}${reporterOutput !== undefined ? 'Reporter Output:\n' + reporterOutput + '\n\n' : ''}${stackException !== undefined ? 'Exception Stack:\n' + stackException : ''}`;
-                        // Base64 encode the mongled text for our API to consume it without complaining
-                        let encodedStepAttachment = Buffer.from(stepAttachment, 'utf-8').toString('base64');
 
                         let stepLog = {
                             order: order,
@@ -135,48 +113,57 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
                             description: methodName,
                             expected_result: methodName,
                             actual_result: methodName,
-                            status: methodStatus,
-                            attachments: [
-                                {
-                                    'name': `${methodName}-${exe_start_date.toString().replace(/:/g,'-')}.txt`,
-                                    'content_type': 'text/plain',
-                                    'data': encodedStepAttachment
-                                }
-                            ]
+                            status: methodStatus
                         };
 
                         testSteps.push(stepLog);
-                        order++;
+
+                        let testLog = {
+                            status: methodStatus,
+                            name: methodName,
+                            attachments: [],
+                            exe_start_date: exe_start_date,
+                            exe_end_date: exe_end_date,
+                            automation_content: automationContent,
+                            note: methodDescription,
+                            module_names: [topLevel, suiteName, className],
+                            test_step_logs: testSteps,
+                            attachments: []
+                        };
+
+                        if (methodStatus == 'FAIL') {
+                            stackMessage = tm.exception.message;
+                            stackException = tm.exception['full-stacktrace'];
+                            let methodAttachment = `Message:\n${stackMessage}\n\nStack Trace:\n${stackException}`;
+                            encodedMethodAttachment = Buffer.from(methodAttachment, 'utf-8').toString('base64');
+                            testLog.attachments.push({
+                                'name': `stack-trace-${methodName}-${exe_start_date.toString().replace(/:/g,'-')}.txt`,
+                                'content_type': 'text/plain',
+                                'data': encodedMethodAttachment
+                            });
+                        }
+
+                        testLogs.push(testLog);
                     } else {
                         console.log(`[INFO]: ${methodName} is a housekeeping task and is not a valid test result.`);
                     }
-                });
-            
-                let exe_end_date = timestamp;
-                exe_end_date.setSeconds(exe_start_date.getSeconds() + (Math.floor(tc.$.time || 0)));
+                }
 
-                let testLog = {
-                    status: methodStatus,
-                    name: className,
-                    attachments: [],
-                    exe_start_date: exe_start_date.toISOString(),
-                    exe_end_date: exe_end_date.toISOString(),
-                    automation_content: suiteName + '#' + className,
-                    module_names: [topLevel, suiteName],
-                    test_step_logs: testSteps
-                };
+                if (testLogs.length > 0) {
+                    let formattedResults = {
+                        "projectId": projectId,
+                        "testcycle": cycleId,
+                        "logs": testLogs
+                    };
 
-                testLogs.push(testLog);
+                    console.log(`[DEBUG]: ${JSON.stringify(formattedResults)}`);
+                    emitEvent('UpdateQTestWithFormattedResults', formattedResults);
 
-            });
-        });
-
-        let formattedResults = {
-            "projectId" : projectId,
-            "testcycle": cycleId,
-            "logs" : testLogs
-        };
-
-        emitEvent('UpdateQTestWithFormattedResults', formattedResults );
+                    await sleep(2000);  // Sleep for 2 seconds after each test class
+                } else {
+                    console.log('[INFO] Class had 0 valid test methods (likely all housekeeping tasks), skipping...');
+                }
+            }
+        }
     });
 };
